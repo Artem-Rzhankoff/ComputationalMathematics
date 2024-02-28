418dfad2bcada08ecc1b43a5d2c5f81d01d490c3
Artem-Rzhankoff <artemr2004@rambler.ru>
2024-02-28 14:16:43 +0300

    feat: implement wave scheme

--- firstTask/main.c
+++ firstTask/main.c
@@ -26,19 +26,19 @@ double randfrom(double min, double max)
 
 void random_padding(double** matrix, size_t size)
 {
-    for (int i = 1; i < size; ++i)
-        for (int j = 1; j < size; ++j)
+    for (int i = 1; i < size + 1; ++i)
+        for (int j = 1; j < size + 1; ++j)
             matrix[i][j] = randfrom(-100.0, 100.0);
 }
 
 void edge_cases_padding(double** matrix, size_t size, edge_cases edge)
 {
     int i;
-    for (i = 0; i < size + 1; ++i) {
+    for (i = 0; i < size + 2; ++i) {
         matrix[0][i] = edge.zero_x[0] + edge.zero_x[1] * i;
         matrix[size][i] = edge.one_x[0] + edge.one_x[1] * i;
     }
-    for (i = 0; i < size + 1; ++i) {
+    for (i = 0; i < size + 2; ++i) {
         matrix[i][0] = edge.zero_y[0] + edge.zero_y[1] * i;
         matrix[i][size] = edge.one_y[0] + edge.one_y[1] * i;
     }
@@ -48,7 +48,7 @@ void edge_cases_padding(double** matrix, size_t size, edge_cases edge)
 double** create_matrix(size_t size, edge_cases edge) 
 {
     double* matrix[size];
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size + 2; ++i) {
         matrix[i] = (double*)malloc(size * sizeof(double));
     }
     // тут наверное можно заполнение ебануть
@@ -64,28 +64,50 @@ void calculate_aproxy(double** matrix, double** f ,size_t size)
 {
     double h = 1 / (size + 1), dmax = 0;
     int i, j;
+    double dm[size]; // !!!?!?
 
     omp_lock_t dmax_lock;
     omp_init_lock (&dmax_lock);
     do {
         dmax = 0;
-#pragma omp parallel for shared(matrix, size, dmax)\
-                         private(i, j, temp, d, dm)
-        for (i = 1; i < size + 1; ++i) { // size ?
-            double dm = 0;
-            for (j = 1; j < size + 1; ++j) {
-
+        for (int wavelength = 1; wavelength < size + 1; ++wavelength) {
+            dm[wavelength] = 0;
+#pragma omp parallel for shared(matrix, wavelength, dm)\
+                         private(i, j, temp, d)
+            for (i=1; i < wavelength + 1; ++i) {
+                j = wavelength + 1 - i;
                 double temp = matrix[i][j];
-                matrix[i][j] = 0.25 * (matrix[i-1][j] + matrix[i+1][j] 
-                + matrix[i][j-1] + matrix[i][j+1] - h * h * f[i][j]);
+
+                matrix[i][j] = 0.25 * (matrix[i-1][j] + matrix[i+1][j] + 
+                    matrix[i][j-1] + matrix[i][j+1] - h * h * f[i][j]);
 
                 double d = fabs(temp - matrix[i][j]);
-                if (dm < d) dm = d;
+                if (dm[i] < d) dm[i] = d;
             }
-            omp_set_lock(&dmax_lock);
-            if (dmax < dm) dmax = dm;
-            omp_unset_lock(&dmax_lock);
         }
+
+        for (int wavelength = size - 1; wavelength > 0; --wavelength) {
+#pragma omp parallel for shared(matrix, wavelength, dm)\
+                         private(i, j, temp, dm)
+            for (i = size - wavelength + 1; i < size + 1; ++i) {
+                j = 2 * size - wavelength - i + 1;
+                double temp = matrix[i][j];
+
+                matrix[i][j] = 0.25 * (matrix[i-1][j] + matrix[i+1][j] + 
+                    matrix[i][j-1] + matrix[i][j+1] - h * h * f[i][j]);
+
+                double d = fabs(temp - matrix[i][j]);
+                if (dm[i] < d) dm[i] = d;
+            }            
+        } // size -> wavelength ??
+#pragma omp parallel for shared(size, dm, dmax)\
+                         private(i)
+        for (i = 1; i < size + 1; ++i) {
+            omp_set_lock(&dmax_lock);
+                if (dm[i] > dmax) dmax = dm[i];
+            omp_unset_lock(&dmax_lock); 
+        }
+
     } while (dmax > EPSILON);
 }
 
